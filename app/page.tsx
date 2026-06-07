baa1e32ae4d11640832a5c84553cfc3e2a4f6733
Kalhara Gunawardhana <kalzmiami@gmail.com>
2026-06-07 13:26:52 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>AI Shopping Agent 🛍️</h1>
+      <p>Type a product to search</p>
+    </div>
+  );
+}
